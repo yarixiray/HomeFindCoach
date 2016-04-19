d3135303b1dcde829e7ccf50c5f11b130ef3b0fb
ILiabogov <mnbmbnm>
2016-04-19 15:19:38 +0300

    Updated version

--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -4,11 +4,8 @@ import org.openqa.selenium.support.PageFactory;
 
 
 public abstract class BasePage {
-
     protected WebDriver driver;
     protected Actions builder;
-
-
     public abstract void open();
 
     public BasePage(WebDriver driver) {
